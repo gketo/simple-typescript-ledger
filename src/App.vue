b37bf73307234ad7ee6da66b8274fe2951124a2a
Guillaume K. <264968328+gketo@users.noreply.github.com>
2026-09-16 09:53:39 +0200

    Transaction View : HTML Statique

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Mon Ledger</h1>
+  <div>Solde : xx €</div>
+  <h2>Transactions</h2>
+  <table>
+    <tr>
+      <th>Date</th>
+      <th>Description</th>
+      <th>Montant</th>
+    </tr>
+    <tr>
+      <td>15/09</td>
+      <td>Salaire</td>
+      <td>+2 000€</td>
+    </tr>
+    <tr>
+      <td>14/09</td>
+      <td>Salaire</td>
+      <td>-80€</td>
+    </tr>
+    <tr>
+      <td>12/09</td>
+      <td>Salaire</td>
+      <td>-670€</td>
+    </tr>
+  </table>
+  <button>[ Ajouter une transaction ]</button>
+</template>
+
+<style scoped></style>
