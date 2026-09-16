0a7bc7a19809d7ae86c9a01f6b9506fcdbe712f2
Guillaume K. <264968328+gketo@users.noreply.github.com>
2026-09-16 22:21:42 +0200

    feat: change basic http server to Fastify server

--- server/Server.ts
+++ server/Server.ts
@@ -1,29 +1,23 @@
-import http from 'http'
+import Fastify from 'fastify'
 
-const server = http.createServer((req, res) => {
-  if (req.method === 'GET') {
-    switch (req.url) {
-      case '/':
-        res.statusCode = 200
-        res.setHeader('Content-Type', 'application/json')
-        res.end(JSON.stringify({ message: 'welcome' }))
-        break
-      case '/transactions':
-        res.statusCode = 200
-        res.setHeader('Content-Type', 'application/json')
-        const body = [{ id: -1, date: new Date(), description: 'test json', amount: 0 }]
-        res.end(JSON.stringify(body))
-        break
-      default:
-        res.statusCode = 404
-        res.setHeader('Content-Type', 'application/json')
-        res.end(JSON.stringify({ error: 'Error 404: not found' }))
-        break
-    }
-  }
+const PORT = 3001
+
+const fastify = Fastify({
+  logger: true,
 })
 
-const PORT = 3000
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`)
+fastify.get('/', function (request, reply) {
+  reply.send({ message: 'welcome2' })
+})
+
+fastify.get('/transactions', function (request, reply) {
+  const body = [{ id: -1, date: new Date(), description: 'test json', amount: 0 }]
+  reply.send(body)
+})
+
+fastify.listen({ port: PORT }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
 })
