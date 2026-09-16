aa62589bbed414521021d6173a244d96cb7f1b7b
Guillaume K. <264968328+gketo@users.noreply.github.com>
2026-09-16 11:38:57 +0200

    feat: added reactive transaction list and balance. Computed, ref, reactive, typescript, vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,44 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+
+interface Transaction {
+  date: Date
+  description: string
+  amount: number
+}
+
+const transactions = ref<Transaction[]>([
+  { date: new Date(2026, 8, 15), description: 'Salaire', amount: 2000 },
+  { date: new Date(2026, 8, 14), description: 'Courses', amount: -80 },
+  { date: new Date(2026, 8, 12), description: 'Loyer', amount: -670 },
+])
+
+const solde = computed(() => {
+  return transactions.value.reduce((acc, trans) => {
+    return acc + trans.amount
+  }, 0)
+})
+</script>
 
 <template>
   <h1>Mon Ledger</h1>
-  <div>Solde : xx €</div>
+  <div>Solde : {{ solde }} €</div>
   <h2>Transactions</h2>
   <table>
-    <tr>
-      <th>Date</th>
-      <th>Description</th>
-      <th>Montant</th>
-    </tr>
-    <tr>
-      <td>15/09</td>
-      <td>Salaire</td>
-      <td>+2 000€</td>
-    </tr>
-    <tr>
-      <td>14/09</td>
-      <td>Salaire</td>
-      <td>-80€</td>
-    </tr>
-    <tr>
-      <td>12/09</td>
-      <td>Salaire</td>
-      <td>-670€</td>
-    </tr>
+    <thead>
+      <tr>
+        <th>Date</th>
+        <th>Description</th>
+        <th>Montant</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="trans in transactions">
+        <td>{{ trans.date.getDate() }}/{{ trans.date.getMonth() + 1 }}</td>
+        <td>{{ trans.description }}</td>
+        <td>{{ trans.amount }}</td>
+      </tr>
+    </tbody>
   </table>
   <button>[ Ajouter une transaction ]</button>
 </template>
